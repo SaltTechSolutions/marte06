10793340042eefadef60e222b40b6c53c4b2758b
Tarki1151 <tarkan.cicek@gmail.com>
2025-08-22 13:35:05 +0300

    kodda temizlik gereksiz kodlar silindi

--- src/types/Branch.ts
+++
@@ -1,9 +0,0 @@
-// src/types/Branch.ts
-import type { Timestamp } from 'firebase/firestore';
-
-export interface Branch {
-  id: string;
-  name: string;
-  description?: string;
-  createdAt?: Timestamp;
-}
